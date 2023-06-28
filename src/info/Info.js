1a6f1bc52eed0b64042ebe5d7a4f3944aa59ccc2
manueloso <emanuelea.rega@itisvoltanapoli.edu.it>
2023-06-28 04:35:19 +0200

    first commit

---
+++ src/info/Info.js
@@ -0,0 +1,127 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Manuel",
+    lastName: "Rega",
+    initials: "MR", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Web Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in Italy'
+        },
+        {
+            emoji: "💼",
+            text: "Student @ Apple Developer Academy"
+        },
+        {
+            emoji: "📧",
+            text: "me@emanuele.tech"
+        }
+    ],
+    socials: [
+        {
+            link: "https://facebook.com",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://instagram.com",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com/manueloso",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hello! I'm Manuel. I'm a student at the Apple Developer Academy in Napoli. I studied CompSci in High School, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
+            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Presentazione PCTO",
+            live: "https://docs.google.com/presentation/d/1NyVXCep_ZKvnRa7HABJRC3WFQAKIg9YpFsukq2NDR0c/edit?usp=sharing", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/manueloso", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "EyeVisionX",
+            live: "https://eyevisionx.com",
+            source: "https://github.com/manueloso",
+            image: mock2
+        },
+        {
+            title: "Sfregio",
+            live: "https://sfregio.com",
+            source: "https://github.com/manueloso",
+            image: mock3
+        }
+    ]
+}
